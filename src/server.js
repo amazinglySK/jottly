3f9cfafbf579efcdb23df138025bf60d4fe59461
amazinglySK <shashwat07ktm@gmail.com>
2022-04-30 21:05:56 +0400

    added custom 404 pages

--- src/server.js
+++ src/server.js
@@ -15,7 +15,9 @@ app.use("/", mainRoutes);
 app.use("/user", userRoutes);
 app.use("/api", apiRoutes);
 app.use("/logs", logRouter);
-
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "/app/not_found.html"));
+});
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
