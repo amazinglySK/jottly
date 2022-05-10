fb6ceb634ed0a7a998a434b219826b01748b2780
amazinglySK <shashwat07ktm@gmail.com>
2022-05-10 20:26:59 +0400

    adding ratelimiting

--- src/server.js
+++ src/server.js
@@ -3,18 +3,26 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 5500;
+const { rateLimit } = require("express-rate-limit");
 const mainRoutes = require("./routes/mainRoutes");
 const userRoutes = require("./routes/userRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 const logRouter = require("./routes/logRoutes");
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 app.use(express.static(path.join(__dirname, "/app")));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/", mainRoutes);
-app.use("/user", userRoutes);
-app.use("/api", apiRoutes);
-app.use("/logs", logRouter);
+app.use("/user", userRoutes, limiter);
+app.use("/api", apiRoutes, limiter);
+app.use("/logs", logRouter, limiter);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "/app/not_found.html"));
 });
